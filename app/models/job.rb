e199933b06d0dd32568531c860e3a15a73e4da3c
Dahfab <fabian.dahlke@gmail.com>
2018-11-15 22:23:41 +0100

    changed slug candidates for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,15 @@ class Job < ApplicationRecord
    belongs_to :company, optional: true
 
    extend FriendlyId
-   friendly_id :position, use: :slugged
+   friendly_id :slug_candidates, use: :slugged
+
+   def slug_candidates
+    [
+      :position,
+      [:position, SecureRandom.hex(3)],
+      [:position, SecureRandom.hex(3), SecureRandom.hex(3)]
+    ]
+  end
 
    accepts_nested_attributes_for :company
 end
