59155b7d82b1a2dcbb4ab3f4f0cb7e2d80357034
Dahfab <fabian.dahlke@gmail.com>
2018-11-15 21:36:59 +0100

    added company slugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_200320) do
+ActiveRecord::Schema.define(version: 2018_11_15_203601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_200320) do
     t.decimal "company_long", precision: 10, scale: 6
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
