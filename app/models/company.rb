7612c5afb221f5b9d70b79ca2bc354e4a8280d83
Dahfab <fabian.dahlke@gmail.com>
2018-11-15 22:25:26 +0100

    changed slug candidates for companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,14 @@
 class Company < ApplicationRecord
     has_many :jobs
 
+    extend FriendlyId
+    friendly_id :slug_candidates, use: :slugged
+ 
+    def slug_candidates
+     [
+       :name,
+       [:name, SecureRandom.hex(3)],
+       [:name, SecureRandom.hex(3), SecureRandom.hex(3)]
+     ]
+   end
 end
