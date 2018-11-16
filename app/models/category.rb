0cb7979f76d09c31251f78a9ff2d54704792ea01
Dahfab <fabian.dahlke@gmail.com>
2018-11-16 14:14:59 +0100

    added slug logic for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,15 @@
 class Category < ApplicationRecord
-    has_many :jobs
+    has_many :job
 
     extend FriendlyId
-    friendly_id :name, use: :slugged
+    friendly_id :name_attr, use: :slugged
+
+    def name_attr
+        if name == "Green-Jobs"
+            "#{name}"
+        else
+            "#{name} jobs"
+        end
+    end
+
 end
