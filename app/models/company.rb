c6ec9fcf9ddc3774aeef8686359e337ea8616bad
Dahfab <fabian.dahlke@gmail.com>
2018-11-19 21:32:01 +0100

    added active_storge + minimagick + local company logo upload

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
     has_many :jobs
+    has_one_attached :logo
 
     extend FriendlyId
     friendly_id :slug_candidates, use: [:slugged, :history]
@@ -10,5 +11,9 @@ class Company < ApplicationRecord
        [:name, SecureRandom.hex(3)],
        [:name, SecureRandom.hex(3), SecureRandom.hex(3)]
      ]
+   end
+
+   def logo_small
+    return self.logo.variant(resize: "100x100")
    end
 end
