29afbf53cbd0f82fcc3883362bb93af2247b03e1
Fabian Dahlke <fabian.dahlke@gmail.com>
2018-11-28 22:26:23 +0100

    added listing sorting on index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
     before_action :initialize_variables, only: :index
     
     def index 
+        @jobs_week = @this_week - @jobs_today
+        @jobs_month = @this_month - @this_week
     end
 
     private
     def initialize_variables 
-        @jobs = Job.all
+        @jobs = Job.all.where("created_at < ?", 1.month.ago).order(created_at: :desc)
+        @jobs_today = Job.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).order(created_at: :desc)
+        @this_week = Job.where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week).order(created_at: :desc)
+        @this_month = Job.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).order(created_at: :desc)
         @category = Category.all
     end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,6 @@ class CategoriesController < ApplicationController
     def show
         @category = Category.find_by_friendly_id(params[:id])
         @category_id = Category.friendly.find(params[:id])
-        @jobs = Job.where(category_id: @category_id)
+        @jobs = Job.where(category_id: @category_id).order(created_at: :desc)
     end
 end
