b7ffc13cfb3222ddbf882c113a551bd7eb30e304
Fabian Dahlke <fabian.dahlke@gmail.com>
2018-11-29 00:20:24 +0100

    moved category_name logic into controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,22 @@
 class CategoriesController < ApplicationController
+    before_action :category_name, only: :show
 
     def index 
     end
 
     def show
-        @category = Category.find_by_friendly_id(params[:id])
         @category_id = Category.friendly.find(params[:id])
         @jobs = Job.where(category_id: @category_id).order(created_at: :desc)
     end
+
+    private
+    def category_name
+        @category = Category.find_by_friendly_id(params[:id])
+        if @category.name =~ /(.+)-jobs/i
+            @category_name = @category.name
+        else
+            @category_name = @category.name + " Jobs"
+        end
+    end
+    
 end
