86478566b7fd95198724f4c81ece5c02ad1fa1c7
Fabian Dahlke <fabian.dahlke@gmail.com>
2019-01-03 21:29:40 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(:name => "GIS", :slug => "gis-jobs")
+Category.create(:name => "Geographie", :slug => "geographie-jobs")
+Category.create(:name => "Geologie", :slug => "geologie-jobs")
+Category.create(:name => "Vermessung", :slug => "vermessung-jobs")
+Category.create(:name => "Green-Jobs", :slug => "green-jobs")
+Category.create(:name => "Sonstige", :slug => "sonstige-jobs")
