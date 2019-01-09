a47fdaa2d022ba23af7c67e43ac9a6924863b81e
Fabian Dahlke <fabian.dahlke@gmail.com>
2019-01-09 20:00:25 +0100

    expire jobs after 45 days

--- app/jobs/expire_job.rb
+++ app/jobs/expire_job.rb
@@ -2,7 +2,7 @@ class ExpireJob
     include SuckerPunch::Job
   
     def perform(job_free, job_highlight)
-        job_free.where("created_at <= ?", 3.days.ago).destroy_all
+        job_free.where("created_at <= ?", 45.days.ago).destroy_all
         job_highlight.where("created_at <= ?", 90.days.ago).destroy_all
     end
 end
